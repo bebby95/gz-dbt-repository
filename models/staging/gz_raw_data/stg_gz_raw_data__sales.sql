78a6aa9278d1190a18990be72cae2d86ac236a9f
garalnick@outlook.com <garalnick@outlook.com>
2025-04-22 12:37:28 +0000

    added tests in yml

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'sales') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        orders_id,
+        CAST(pdt_id AS INT64) AS product_id,
+        revenue,
+        quantity,
+        CONCAT(pdt_id, '_', orders_id) AS primary_key
+    from source
+
+)
+
+select * from renamed
