ce229b26fa24b71df553fc1ee0e03917f1c7e19a
garalnick@outlook.com <garalnick@outlook.com>
2025-04-22 12:39:01 +0000

    same update as below

--- models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
@@ -11,7 +11,7 @@ renamed as (
     select
         date_date,
         orders_id,
-        CAST(pdt_id AS INT64),
+        CAST(pdt_id AS INT64) AS product_id,
         revenue,
         quantity,
         CONCAT(pdt_id, '_', orders_id) AS primary_key
