82a456b86bba2562300ca643efbd20b42f03824c
garalnick@outlook.com <garalnick@outlook.com>
2025-04-23 11:53:32 +0000

    int_campaigns_day created, aggregates marketing data day by day

---
+++ models/intermediate/int_campaigns_day.sql
@@ -0,0 +1,6 @@
+SELECT  date_date
+, SUM(ads_cost) AS ads_cost
+, CAST(SUM(impression) AS INT64) AS total_impressions
+, CAST(SUM(click) AS INT64) AS total_clicks
+FROM {{ ref("int_campaigns") }} AS campaigns
+GROUP BY date_date
