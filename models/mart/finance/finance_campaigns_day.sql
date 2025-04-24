4b1a7b423dec00e07db6028972c460ef571678a0
garalnick@outlook.com <garalnick@outlook.com>
2025-04-24 07:48:32 +0000

    dfgsderfa

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -10,13 +10,13 @@ SELECT date_date
 , avg_basket AS average_basket
 , operational_margin
 , ads_cost
-, total_impressions AS ads_impression
-, total_clicks AS ads_clicks
-, CAST(total_qty_sold AS INT64) AS quantity
-, total_revenue AS revenue
-, total_purchase_cost AS purchase_cost
+, impressions AS ads_impression
+, clicks AS ads_clicks
+, CAST(qty_sold AS INT64) AS quantity
+, revenue AS revenue
+, purchase_cost AS purchase_cost
 , margin
-, total_shipping_fee AS shipping_fee
-, total_log_cost AS log_cost
+, shipping_fee AS shipping_fee
+, log_cost AS log_cost
 , ship_cost
 FROM joined
